c5660551b19623e667c6bfd016f436dc39365f5e
fujiyama <inapapapa@gmail.com>
2021-06-17 20:10:47 +0900

    office

--- src/main/java/com/github/fujiyamakazan/zabuton/util/file/ZipUtils.java
+++ src/main/java/com/github/fujiyamakazan/zabuton/util/file/ZipUtils.java
@@ -45,7 +45,7 @@ public class ZipUtils {
             }
 
         } catch (IOException e) {
-            throw new RuntimeException();
+            throw new RuntimeException(e);
         }
     }
 
