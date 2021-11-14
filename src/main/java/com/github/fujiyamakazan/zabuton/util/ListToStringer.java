1187cb030ab7ea77f01d54953af6126e9bf037f4
fujiyama <inapapapa@gmail.com>
2021-11-15 08:29:29 +0900

    @office

--- src/main/java/com/github/fujiyamakazan/zabuton/util/ListToStringer.java
+++ src/main/java/com/github/fujiyamakazan/zabuton/util/ListToStringer.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.github.fujiyamakazan.zabuton.util;
 
 import java.util.Arrays;
@@ -39,45 +38,5 @@ public class ListToStringer {
     }
 
 }
-=======
-package com.github.fujiyamakazan.zabuton.util;
-
-import java.util.Arrays;
-import java.util.List;
 
 
-/**
- * ListをStringに変換する
- * @author fujiyama
- */
-public class ListToStringer {
-
-    public static String convert(List<?> list) {
-        //return convert(list, "\n");
-        return convert(list, ","); // 2021.9.12
-    }
-
-    /**
-     * リストをdelimiterを区切り文字として文字列に変換します。
-     */
-    public static String convert(List<?> list, String delimiter) {
-        StringBuilder sb = new StringBuilder();
-        for (Object e :list) {
-            if (sb.length() > 0) {
-                sb.append(delimiter);
-            }
-            if (e == null) {
-                sb.append("[null]");
-            } else {
-                sb.append(e.toString());
-            }
-        }
-        return sb.toString();
-    }
-
-    public static String convert(String[] strings) {
-        return convert(Arrays.asList(strings));
-    }
-
-}
->>>>>>> refs/remotes/origin/master
--- src/main/java/com/github/fujiyamakazan/zabuton/util/RetryWorker.java
+++ src/main/java/com/github/fujiyamakazan/zabuton/util/RetryWorker.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.github.fujiyamakazan.zabuton.util;
 
 import java.io.Serializable;
@@ -35,42 +34,5 @@ public abstract class RetryWorker implements Serializable {
 
     protected abstract void recovery();
 
-=======
-package com.github.fujiyamakazan.zabuton.util;
 
-import java.io.Serializable;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public abstract class RetryWorker implements Serializable {
-    private static final long serialVersionUID = 1L;
-
-    private static final Logger log = LoggerFactory.getLogger(RetryWorker.class);
-
-    public void start() {
-        int count = 0;
-        while (true) {
-            if (count > 0) {
-                log.debug("RetryWorker:" + count);
-            }
-            try {
-                run();
-                break;
-            } catch (Exception e) {
-                log.debug(ThrowableToString.convertToString(e));
-
-                if (count++ > 10) {
-                    throw new RuntimeException(e);
-                }
-                recovery();
-            }
-        }
-    }
-
-    protected abstract void run();
-
-    protected abstract void recovery();
-
->>>>>>> refs/remotes/origin/master
 }
