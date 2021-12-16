78b7d73a9de781a2e6e54a0c907c38572c66731e
fujiyama <inapapapa@gmail.com>
2021-12-16 17:54:54 +0900

    @office

--- src/main/java/com/github/fujiyamakazan/zabuton/util/text/TextMerger.java
+++ src/main/java/com/github/fujiyamakazan/zabuton/util/text/TextMerger.java
@@ -1,11 +1,14 @@
 package com.github.fujiyamakazan.zabuton.util.text;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.wicket.util.lang.Generics;
+
+import com.opencsv.CSVParser;
 
 /**
  * テキストファイルの追記処理をします。
@@ -17,7 +20,7 @@ import org.apache.wicket.util.lang.Generics;
  * ・マスターがあるにもかかわらず、最後に処理したテキストにもマスター追加済みレコードと一致するものが無ければ、
  * 遡及回数の不足と考えられる。処理を中断し、警告をする。
  */
-public class TextMerger implements Serializable {
+public abstract class TextMerger implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
@@ -26,12 +29,24 @@ public class TextMerger implements Serializable {
      */
     public static void main(String[] args) {
 
-        File fileMaster = new File("C:\\tmp\\textMaster.txt");
+        int year = 2021;
+        File fileMaster = new File("C:\\tmp\\textMaster" + year + ".txt");
         List<File> additionlFiles = Generics.newArrayList();
         additionlFiles.add(new File("C:\\tmp\\text追加2.txt")); // 処理は最新の追加テキストから順に行う。
         additionlFiles.add(new File("C:\\tmp\\text追加1.txt"));
 
-        TextMerger textMerger = new TextMerger(fileMaster);
+        TextMerger textMerger = new TextMerger(fileMaster) {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            protected boolean isAvailableLine(String line) {
+                try {
+                    return new CSVParser().parseLine(line)[0].equals(String.valueOf(year));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
         for (File additionalFile : additionlFiles) { // 遡及回数は一定の上限値を決める。（無限ループの防止。）
 
             Utf8Text utf8Text = new Utf8Text(additionalFile);
@@ -52,6 +67,8 @@ public class TextMerger implements Serializable {
 
     }
 
+    protected abstract boolean isAvailableLine(String line);
+
     private final File masterText;
     private final List<String> masterLines = Generics.newArrayList();
     private final List<String> buffer = Generics.newArrayList();
@@ -70,9 +87,15 @@ public class TextMerger implements Serializable {
         this.masterText = masterText;
         for (String line : new Utf8Text(masterText).readLines()) {
             line = line.trim();
-            if (StringUtils.isEmpty(line) == false) {
-                masterLines.add(line);
+            if (isSkipLine(line)) {
+                continue;
             }
+
+            if (masterLines.contains(line)) {
+                throw new RuntimeException("重複するレコードを持つ不正なマスターです。" + line);
+            }
+
+            masterLines.add(line);
 
         }
     }
@@ -82,9 +105,8 @@ public class TextMerger implements Serializable {
      * 遡及回数の不足と考えられる。処理を中断し、警告をする。
      */
     private boolean isFinish() {
-        return masterLines.isEmpty() == false && existMaster;
+        return masterLines.isEmpty() || existMaster;
     }
-
 
     /**
      * マスターにある同一のレコード以外をbufferに仮保存する。
@@ -94,7 +116,7 @@ public class TextMerger implements Serializable {
         List<String> joins = Generics.newArrayList();
         for (String additionalLine : additionalText.split("\n")) {
             additionalLine = additionalLine.trim();
-            if (StringUtils.isEmpty(additionalLine)) {
+            if (isSkipLine(additionalLine)) {
                 continue;
             }
             if (masterLines.contains(additionalLine)) {
@@ -132,6 +154,13 @@ public class TextMerger implements Serializable {
         master.writeLines(buffer, true);
 
         buffer.clear(); // 使用済みの為削除
+    }
+
+    protected boolean isSkipLine(String line) {
+        if (StringUtils.isEmpty(line)) {
+            return false;
+        }
+        return isAvailableLine(line) == false;
     }
 
 }
