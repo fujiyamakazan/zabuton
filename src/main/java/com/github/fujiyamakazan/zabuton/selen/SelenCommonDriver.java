5256a502a1fe3ed15dd5ec8a7c1a1bec7595afeb
fujiyama <inapapapa@gmail.com>
2023-01-04 18:13:59 +0900

    @office

--- src/main/java/com/github/fujiyamakazan/zabuton/selen/SelenCommonDriver.java
+++ src/main/java/com/github/fujiyamakazan/zabuton/selen/SelenCommonDriver.java
@@ -30,94 +30,206 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import com.github.fujiyamakazan.zabuton.util.EnvUtils;
 import com.github.fujiyamakazan.zabuton.util.HttpConnector;
 import com.github.fujiyamakazan.zabuton.util.RetryWorker;
+import com.github.fujiyamakazan.zabuton.util.ThrowableToString;
 import com.github.fujiyamakazan.zabuton.util.exec.RuntimeExc;
 import com.github.fujiyamakazan.zabuton.util.file.ZipUtils;
 
 public abstract class SelenCommonDriver implements Serializable {
 
     private static final org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger(SelenCommonDriver.class);
-
-    private static final String CHROMEDRIVER_EXE = "chromedriver.exe";
+    private static final long serialVersionUID = 1L;
 
     public static final int DEFAULT_TIMEOUT = 5;
 
     private static final By BODY = By.cssSelector("body");
 
-    private static final long serialVersionUID = 1L;
-
     private final transient WebDriver originalDriver;
 
-    /**
-     * Seleniumの一時ファイルを自動削除する場合はtrueを指定します。
-     */
+    /** Seleniumの一時ファイルを自動削除する場合はtrueを指定します。 */
     public static boolean deleteTemp = true;
+
+    /**
+     * Webドライバのファクトリです。
+     */
+    private abstract class DriverFactory implements Serializable {
+        private static final long serialVersionUID = 1L;
+
+        public File driverFile;
+
+        public DriverFactory(File driverDir) {
+            this.driverFile = new File(driverDir, getDriverFileName());
+        }
+
+        public File getDriverFile() {
+            return driverFile;
+        }
+
+        /**
+         * ドライバの実行ファイルの名称を返します。
+         */
+        public abstract String getDriverFileName();
+
+        /**
+         * ドライバの実行ファイルを取得できるURLを返します。
+         */
+        public abstract String getDriverUrl();
+
+        /**
+         * ドライバの実行ファイルを取得します。
+         */
+        public abstract void download();
+
+        /**
+         * ドライバの実行ファイルからWebドライバオブジェクトを生成します。
+         */
+        public abstract WebDriver create(File downloadDefaultDir);
+
+        /**
+         * ドライバのタスクを強制終了します。
+         */
+        public void killTask() {
+            RuntimeExc.executeCmd("taskkill /im "+getDriverFileName()+" /f");
+        }
+
+        /**
+         * 例外情報から不正なバージョンの発生を検知します。
+         */
+        public abstract boolean occurredIllegalVersionDetected(Exception e);
+
+
+
+    }
+
+    /**
+     * GoogleChrome用のWebドライバのファクトリです。
+     */
+    private class ChoromeDriverOptions extends DriverFactory {
+        private static final long serialVersionUID = 1L;
+
+        public ChoromeDriverOptions(File driverDir) {
+            super(driverDir);
+        }
+
+        @Override
+        public String getDriverFileName() {
+            return "chromedriver.exe";
+        }
+
+        @Override
+        public String getDriverUrl() {
+            return "https://chromedriver.chromium.org/downloads";
+        }
+
+        @Override
+        public void download() {
+            String html = HttpConnector.byBody(getDriverUrl());
+            int count = 0;
+            for (String line : html.split("\n")) {
+                if (StringUtils.startsWith(line, "If you are using Chrome version ")) {
+                    if (count == 0) { // 1行目は飛ばす（現行バージョンではない可能性が高い。）
+                        count++;
+                        continue;
+                    }
+                    String keyword = "ChromeDriver ";
+                    String ver = line.substring(line.lastIndexOf(keyword) + keyword.length());
+                    String url = String.format("https://chromedriver.storage.googleapis.com/%s/chromedriver_win32.zip",
+                        ver);
+                    File zip = new File(driverFile.getAbsolutePath() + ".zip");
+                    HttpConnector.download(url, zip);
+                    LOGGER.debug(url);
+                    new ZipUtils.UnzipTask(zip) {
+                        private static final long serialVersionUID = 1L;
+
+                        @Override
+                        protected void runByEntry(String name, File file) {
+                            try {
+                                FileUtils.copyFile(file, new File(driverFile.getParentFile(), name));
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                    }.start();
+                    break;
+                }
+            }
+
+        }
+
+        @Override
+        public WebDriver create(File downloadDefaultDir) {
+
+            /* Choromeの環境変数を設定します。 */
+            System.setProperty("webdriver.chrome.driver", driverFile.getAbsolutePath());
+
+            /* ダウンロードフォルダを指定します。 */
+            HashMap<String, Object> prefes = new HashMap<String, Object>();
+            prefes.put("profile.default_content_settings.popups", 0);
+            prefes.put("download.default_directory", downloadDefaultDir.getAbsolutePath());
+            ChromeOptions options = new ChromeOptions();
+            options.setExperimentalOption("prefs", prefes);
+
+            /* Webドライバのインスタンスを返します。*/
+            return new ChromeDriver(options);
+        }
+
+
+        @Override
+        public boolean occurredIllegalVersionDetected(Exception e) {
+            return e instanceof SessionNotCreatedException
+                && e.getMessage().contains("This version of ChromeDriver only supports Chrome version");
+        }
+
+    }
 
     /**
      * コンストラクタです。Webドライバを生成ます。
      */
     public SelenCommonDriver() {
-        //this.originalDriver = createDriver();
 
-        final File driverFile = new File(getDriverDir(), CHROMEDRIVER_EXE);
-        final File dir = getDownloadDir();
+        /* デフォルトではGoogleChromeのWebドライバを生成します。 */
+        DriverFactory factory = new ChoromeDriverOptions(getDriverDir());
+
+        final File driverFile = factory.getDriverFile();
 
         if (driverFile.exists() == false) {
 
-            /* ドライバをダウンロードします。 */
-            downloadChoromeDriver(driverFile);
+            /* ドライバのファイルをダウンロードします。 */
+            factory.download();
 
+            /* ダウンロードが失敗したら例外情報として返します。 */
             if (driverFile.exists() == false) {
-                throw new RuntimeException("WebDriverが次の場所にありません。ダウンロードに失敗しました。"
-                    + driverFile.getAbsolutePath()
-                    + " [https://chromedriver.chromium.org/]からダウロードしてください。");
+                throw new RuntimeException(
+                    "ドライバのファイルが" + driverFile.getAbsolutePath() + "にありません。"
+                    + " [" + factory.getDriverUrl() + "]からダウンロードしてください。");
             }
+
         }
 
-        System.setProperty("webdriver.chrome.driver", driverFile.getAbsolutePath());
-
-        /* ダウンロードフォルダ指定 */
-        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
-        chromePrefs.put("profile.default_content_settings.popups", 0);
-        chromePrefs.put("download.default_directory",
-            dir.getAbsolutePath());
-
-        ChromeOptions options = new ChromeOptions();
-        options.setExperimentalOption("prefs", chromePrefs);
         WebDriver driver;
         try {
-            driver = new ChromeDriver(options);
+            driver = factory.create(getDownloadDir());
 
         } catch (Exception e) {
-            LOGGER.debug(e.getClass().getName() + "が発生。");
-            LOGGER.debug(e.getMessage());
-            if (e instanceof SessionNotCreatedException
-                && e.getMessage()
-                    .contains("This version of ChromeDriver only supports Chrome version")) {
+            LOGGER.error(ThrowableToString.convertToString(e));
 
-                /* バージョン違い */
-                //                throw new RuntimeException(
-                //                    "WebDriverを更新してください。"
-                //                        + driverFile.getAbsolutePath()
-                //                        + " [https://chromedriver.chromium.org/]からダウロードしてください。",
-                //                    e);
-                /*
-                 * ファイル削除実施
-                 */
+            if (factory.occurredIllegalVersionDetected(e)) {
+                /* ドライバファイルのバージョン不正を検知したときの処理 */
                 try {
-                    killDriver();
+                    factory.killTask();
                     Thread.sleep(1000);
-                    Files.deleteIfExists(Path.of(driverFile.getAbsolutePath()));
+                    Files.deleteIfExists(Path.of(driverFile.getAbsolutePath())); // ファイル削除
                 } catch (Exception deleteException) {
                     throw new RuntimeException("削除失敗", deleteException);
                 }
                 throw new RuntimeException(
-                    "WebDriverのバージョンが不正のため差k叙しました。再度実行すると、新しいファイルをダウンロードします。", e);
+                    "ドライバのファイルがバージョンが不正のため削除しました。"
+                        + "再度実行すると、新しいファイルをダウンロードします。",
+                    e);
 
             } else {
                 throw new RuntimeException(e);
             }
         }
-        //
 
         onInitialize(driver);
 
@@ -131,61 +243,6 @@ public abstract class SelenCommonDriver implements Serializable {
     protected void onInitialize(WebDriver driver) {
         /* 暗黙的な待機時間を設定します。 */
         driver.manage().timeouts().implicitlyWait(DEFAULT_TIMEOUT, TimeUnit.SECONDS);
-    }
-
-    //    public static void main(String[] args) {
-    //        //downloadChoromeDriver(new File(EnvUtils.getAppData("chabudai"), CHROMEDRIVER_EXE));
-    //        SelenCommonDriver driver = new SelenCommonDriver() {
-    //
-    //            private static final long serialVersionUID = 1L;
-    //
-    //            @Override
-    //            protected File getDriverDir() {
-    //                return EnvUtils.getAppData("chabudai");
-    //            }
-    //
-    //            @Override
-    //            protected File getDownloadDir() {
-    //                return new File(EnvUtils.getAppData("chabudai"), "download");
-    //            }
-    //
-    //        };
-    //        driver.get("http://google.co.jp");
-    //
-    //    }
-
-    private static void downloadChoromeDriver(File downloadFile) {
-        /* ダウンロード */
-        String html = HttpConnector.byBody("https://chromedriver.chromium.org/downloads");
-        int count = 0;
-        for (String line : html.split("\n")) {
-            if (StringUtils.startsWith(line, "If you are using Chrome version ")) {
-                if (count == 0) { // 1行目は飛ばす（現行バージョンではない可能性が高い。）
-                    count++;
-                    continue;
-                }
-                String keyword = "ChromeDriver ";
-                String ver = line.substring(line.lastIndexOf(keyword) + keyword.length());
-                String url = String.format("https://chromedriver.storage.googleapis.com/%s/chromedriver_win32.zip",
-                    ver);
-                File zip = new File(downloadFile.getAbsolutePath() + ".zip");
-                HttpConnector.download(url, zip);
-                LOGGER.debug(url);
-                new ZipUtils.UnzipTask(zip) {
-                    private static final long serialVersionUID = 1L;
-
-                    @Override
-                    protected void runByEntry(String name, File file) {
-                        try {
-                            FileUtils.copyFile(file, new File(downloadFile.getParentFile(), name));
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                }.start();
-                break;
-            }
-        }
     }
 
     /**
@@ -470,13 +527,6 @@ public abstract class SelenCommonDriver implements Serializable {
                 f.delete();
             }
         }
-    }
-
-    /**
-     * プロセスを削除します。
-     */
-    public static void killDriver() {
-        RuntimeExc.executeCmd("taskkill /im chromedriver.exe /f");
     }
 
     /**
