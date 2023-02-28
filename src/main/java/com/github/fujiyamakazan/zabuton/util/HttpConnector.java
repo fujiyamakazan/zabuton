a39cb517fd629216c5bf064bdc4c9dc9fb42e0b4
fujiyama <inapapapa@gmail.com>
2023-02-28 18:11:13 +0900

    @office

--- src/main/java/com/github/fujiyamakazan/zabuton/util/HttpConnector.java
+++ src/main/java/com/github/fujiyamakazan/zabuton/util/HttpConnector.java
@@ -1,14 +1,13 @@
 package com.github.fujiyamakazan.zabuton.util;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
 import java.net.InetSocketAddress;
 import java.net.ProxySelector;
-import java.net.URI;
 import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpRequest.Builder;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.Duration;
@@ -19,18 +18,24 @@ import java.util.Map;
 import javax.servlet.http.Cookie;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.apache.wicket.util.lang.Generics;
 
 /**
  * HTTP(S)接続をします。
+ *
+ * TODO リファクタリング
  *
  * @author fujiyama
  */
@@ -81,89 +86,138 @@ public class HttpConnector implements Serializable {
      * GETでリクエストします。
      */
     public String get(String url) {
-        HttpClient cl = buildClient();
+//        HttpClient cl = buildClient();
+//
+//        Builder reqBuilder = HttpRequest.newBuilder().uri(URI.create(url));
+//        /* Cookieを設定します。 */
+//        for (Cookie cookie : cookies) {
+//            reqBuilder = reqBuilder.setHeader("Cookie", cookie.getName() + "=" + cookie.getValue());
+//        }
+//        /* User-Agentを設定します。*/
+//        if (userAgent != null) {
+//            reqBuilder = reqBuilder.setHeader("User-Agent", userAgent);
+//        }
+//        HttpRequest req = reqBuilder.build();
+//
+//        HttpResponse<String> res;
+//        try {
+//            res = cl.send(req, HttpResponse.BodyHandlers.ofString());
+//            checkStatuCode(url, res);
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        /* Cookieを取得します。 */
+//        setCookies(res.headers().allValues("set-cookie"));
+//
+//        return res.body();
 
-        Builder reqBuilder = HttpRequest.newBuilder().uri(URI.create(url));
-        /* Cookieを設定します。 */
-        for (Cookie cookie : cookies) {
-            reqBuilder = reqBuilder.setHeader("Cookie", cookie.getName() + "=" + cookie.getValue());
-        }
-        /* User-Agentを設定します。*/
-        if (userAgent != null) {
-            reqBuilder = reqBuilder.setHeader("User-Agent", userAgent);
-        }
-        HttpRequest req = reqBuilder.build();
 
-        HttpResponse<String> res;
-        try {
-            res = cl.send(req, HttpResponse.BodyHandlers.ofString());
-            checkStatuCode(url, res);
+        HttpGet req = getRequest(url);
+        try (final CloseableHttpClient cl = HttpClients.createDefault();
+            final CloseableHttpResponse res = cl.execute(req)) {
+
+            /* ステータスコードをチェックします。 */
+            checkStatuCode(url, res.getStatusLine().getStatusCode());
+
+            /* cookieを引き継ぎます。 */
+            setCookies(res.getHeaders("set-cookie"));
+
+            /* 結果を返します。 */
+            return EntityUtils.toString(res.getEntity());
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
-        /* Cookieを取得します。 */
-        setCookies(res.headers().allValues("set-cookie"));
-
-        return res.body();
-
-        //非同期
-        //BodyHandler<String> responseBodyHandler = HttpResponse.BodyHandlers.ofString();
-        //CompletableFuture<HttpResponse<String>> future = client.sendAsync(request, responseBodyHandler);
-        ///* thenAccept・・・戻り値がないLambdaを処理 */
-        //future.thenAccept(res -> {
-        //    System.out.println(res.body());
-        //});
-        //Thread.sleep(3000);
     }
+
+
 
     /**
      * POSTでリクエストします。
      */
     public String post(String url, List<KeyValue> params) {
 
-        HttpClient cl = buildClient();
+        HttpPost  req = getRequestPost(url);
 
-        StringBuilderLn sb = new StringBuilderLn("&");
+        List<BasicNameValuePair> formparams = Generics.newArrayList();
         for (KeyValue kv : params) {
-            sb.appendLn(kv.getKey() + "=" + kv.getValue());
+            formparams.add(new BasicNameValuePair(kv.getKey(), kv.getValue()));
         }
-        String strPostparam = sb.toString();
+        UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams, StandardCharsets.UTF_8);
+        req.setEntity(entity);
 
-        java.net.http.HttpRequest.Builder reqBuilder = HttpRequest.newBuilder()
-            .uri(URI.create(url))
-            .header("Content-Type", "application/x-www-form-urlencoded")
-            .POST(HttpRequest.BodyPublishers.ofString(strPostparam));
+        try (final CloseableHttpClient cl = HttpClients.createDefault();
+            final CloseableHttpResponse res = cl.execute(req)) {
 
-        /* Cookieを設定します。 */
-        for (Cookie cookie : cookies) {
-            reqBuilder = reqBuilder.setHeader("Cookie", cookie.getName() + "=" + cookie.getValue());
-        }
-        /* User-Agentを設定します。*/
-        if (userAgent != null) {
-            reqBuilder = reqBuilder.setHeader("User-Agent", userAgent);
-        }
+            /* ステータスコードをチェックします。 */
+            checkStatuCode(url, res.getStatusLine().getStatusCode());
 
-        HttpRequest req = reqBuilder.build();
-        HttpResponse<String> res;
-        try {
-            res = cl.send(req, HttpResponse.BodyHandlers.ofString());
-            checkStatuCode(url, res);
+            /* cookieを引き継ぎます。 */
+            setCookies(res.getHeaders("set-cookie"));
+
+            /* 結果を返します。 */
+            return EntityUtils.toString(res.getEntity());
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
-        /* Cookieを取得します。 */
-        setCookies(res.headers().allValues("set-cookie"));
-        String body = res.body();
-        return body;
+//        HttpClient cl = buildClient();
+//
+//        StringBuilderLn sb = new StringBuilderLn("&");
+//        for (KeyValue kv : params) {
+//            sb.appendLn(kv.getKey() + "=" + kv.getValue());
+//        }
+//        String strPostparam = sb.toString();
+//
+//        java.net.http.HttpRequest.Builder reqBuilder = HttpRequest.newBuilder()
+//            .uri(URI.create(url))
+//            .header("Content-Type", "application/x-www-form-urlencoded")
+//            .POST(HttpRequest.BodyPublishers.ofString(strPostparam));
+//
+//        /* Cookieを設定します。 */
+//        for (Cookie cookie : cookies) {
+//            reqBuilder = reqBuilder.setHeader("Cookie", cookie.getName() + "=" + cookie.getValue());
+//        }
+//        /* User-Agentを設定します。*/
+//        if (userAgent != null) {
+//            reqBuilder = reqBuilder.setHeader("User-Agent", userAgent);
+//        }
+//
+//        HttpRequest req = reqBuilder.build();
+//        HttpResponse<String> res;
+//        try {
+//            res = cl.send(req, HttpResponse.BodyHandlers.ofString());
+//            checkStatuCode(url, res);
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        /* Cookieを取得します。 */
+//        setCookies(res.headers().allValues("set-cookie"));
+//        String body = res.body();
+//        return body;
     }
 
     /**
      * ファイルをダウンロードします。
      */
     public void download(String url, File to) {
+        HttpGet req = getRequest(url);
+        try (final CloseableHttpClient cl = HttpClients.createDefault();
+            final CloseableHttpResponse res = cl.execute(req)) {
 
+            checkStatuCode(url, res.getStatusLine().getStatusCode());
+            final HttpEntity entity = res.getEntity();
+            Files.write(Paths.get(to.getAbsolutePath()),
+                entity == null ? new byte[0] : EntityUtils.toByteArray(entity));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private HttpGet getRequest(String url) {
         HttpGet req = new HttpGet(url);
         org.apache.http.client.config.RequestConfig.Builder configBuilder = RequestConfig.custom();
 
@@ -190,18 +244,37 @@ public class HttpConnector implements Serializable {
         if (userAgent != null) {
             req.addHeader("User-Agent", userAgent);
         }
+        return req;
+    }
 
-        try (final CloseableHttpClient cl = HttpClients.createDefault();
-            final CloseableHttpResponse res = cl.execute(req)) {
+    private HttpPost getRequestPost(String url) {
+        HttpPost req = new HttpPost(url);
+        org.apache.http.client.config.RequestConfig.Builder configBuilder = RequestConfig.custom();
 
-            checkStatuCode(url, res.getStatusLine().getStatusCode());
-            final HttpEntity entity = res.getEntity();
-            Files.write(Paths.get(to.getAbsolutePath()),
-                entity == null ? new byte[0] : EntityUtils.toByteArray(entity));
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        /* プロキシを設定します。 */
+        if (proxyAddress != null) {
+            HttpHost proxy = new HttpHost(proxyAddress.getHostName(), proxyAddress.getPort());
+            configBuilder = configBuilder.setProxy(proxy);
         }
+
+        /* タイムアウトを設定します。*/
+        if (this.timeout != null) {
+            configBuilder = configBuilder.setConnectTimeout(timeout);
+        }
+
+        RequestConfig config = configBuilder.build();
+        req.setConfig(config);
+
+        /* Cookieを設定します。 */
+        for (Cookie cookie : cookies) {
+            req.addHeader("Cookie", cookie.getName() + "=" + cookie.getValue());
+        }
+
+        /* User-Agentを設定します。*/
+        if (userAgent != null) {
+            req.addHeader("User-Agent", userAgent);
+        }
+        return req;
     }
 
     private static void checkStatuCode(String url, HttpResponse<String> res) {
@@ -218,6 +291,14 @@ public class HttpConnector implements Serializable {
         if (code >= 400) {
             throw new RuntimeException("StatuCode: " + code);
         }
+    }
+
+    private void setCookies(Header[] headers) {
+        List<String> setCookies = Generics.newArrayList();
+        for (Header header: headers) {
+            setCookies.add(header.getValue());
+        }
+        setCookies(setCookies);
     }
 
     private void setCookies(List<String> setCookies) {
@@ -279,6 +360,31 @@ public class HttpConnector implements Serializable {
         return cl;
     }
 
+    /**
+     * 動作確認をします。
+     */
+    public static void main(String[] args) throws IOException {
+
+        final HttpConnector connectorNoProxy = new HttpConnector();
+        connectorNoProxy.get("http://プロキシ未設定で接続するサイト");
+
+        final HttpConnector connector = new HttpConnector("proxyホスト", 8080);
+        List<KeyValue> postParams = Generics.newArrayList();
+        postParams.add(new KeyValue("Postパラメータ１", "Postパラメータ１の値"));
+        postParams.add(new KeyValue("Postパラメータ２", "Postパラメータ２の値"));
+        String html;
+        html = connector.post("ログイン画面URL", postParams);
+        html = connector.get("処理画面URL");
+        LOGGER.debug(html);
+
+        /* ファイルダウンロードの検査 */
+        File file = File.createTempFile(HttpConnector.class.getSimpleName(), ".jpg");
+        file.deleteOnExit();
+        connector.download("https://cover.openbd.jp/9784087474398.jpg", file);
+
+        LOGGER.debug(file.getAbsolutePath());;
+
+    }
 
 
 }
