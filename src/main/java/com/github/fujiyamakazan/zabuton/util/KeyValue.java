0ead4fe908bf106ea0318c2d555d71b97beaa248
fujiyama <inapapapa@gmail.com>
2023-08-14 20:04:46 +0900

    @office

--- src/main/java/com/github/fujiyamakazan/zabuton/util/KeyValue.java
+++ src/main/java/com/github/fujiyamakazan/zabuton/util/KeyValue.java
@@ -2,35 +2,72 @@ package com.github.fujiyamakazan.zabuton.util;
 
 import java.io.Serializable;
 
-public class KeyValue implements Serializable {
+/**
+ * KeyとValueをペアで保持するオブジェクト。保持する値はStringに限定します。
+ *
+ * Pair&lt;String, String&gt;へ移行します。
+ *
+ * @author k_inaba
+ */
+public class KeyValue extends KeyValueObj<String, String> implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    private String key;
+    //private String key;
 
-    private String value;
+    //private String value;
 
-    public KeyValue() {
-    }
+    //public KeyValue() {
+    //}
 
+    //private final Pair<String, String> keyValue;
+
+    /**
+     * コンストラクタです。valueを指定しません。
+     */
     public KeyValue(String key) {
-        this.key = key;
+        //this.key = key;
+        //this.keyValue = MutablePair.of(key, null);
+
+        super(key);
     }
 
+    /**
+     * コンストラクタです。
+     */
     public KeyValue(String key, String value) {
-        this.key = key;
-        this.value = value;
+        //this.key = key;
+        //this.value = value;
+        //this.keyValue = MutablePair.of(key, value);
+
+        super(key, value);
     }
 
-    public String getKey() {
-        return this.key;
-    }
+    //    public String getKey() {
+    //        //return this.key;
+    //        return this.keyValue.getKey();
+    //    }
+    //
+    //    public String getValue() {
+    //        //return this.value;
+    //        return this.keyValue.getValue();
+    //    }
+    //
+    //    public void setValue(String value) {
+    //        //this.value = value;
+    //        this.keyValue.setValue(value);
+    //    }
 
-    public String getValue() {
-        return this.value;
-    }
+    /**
+     * 単体テストをします。
+     */
+    public static void main(String[] args) {
 
-    public void setValue(String value) {
-        this.value = value;
+        KeyValue kv = new KeyValue("key", "val");
+        kv.setValue("val2");
+
+        System.out.println(kv.getKey());
+        System.out.println(kv.getValue());
+
     }
 
 }
--- src/main/java/com/github/fujiyamakazan/zabuton/util/KeyValueObj.java
+++ src/main/java/com/github/fujiyamakazan/zabuton/util/KeyValueObj.java
@@ -2,34 +2,59 @@ package com.github.fujiyamakazan.zabuton.util;
 
 import java.io.Serializable;
 
+import org.apache.commons.lang3.tuple.MutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+
+/**
+ * KeyとValueをペアで保持するオブジェクト。
+ *
+ * Pair&lt;Object, Object&gt;へ移行します。
+ * @author k_inaba
+ */
 public class KeyValueObj<K, V> implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    private K key;
-    private V value;
+    //private K key;
+    //private V value;
 
-    public KeyValueObj() {
+    //public KeyValueObj() {
+    //}
+
+    private final Pair<K, V> keyValue;
+
+    /**
+     * コンストラクタです。
+     */
+    public KeyValueObj(K key, V value) {
+        //this.key = key;
+        //this.value = value;
+        this.keyValue = MutablePair.of(key, value);
     }
 
-    public KeyValueObj(K key, V value) {
-        this.key = key;
-        this.value = value;
+    /**
+     * コンストラクタです。valueを指定しません。
+     */
+    public KeyValueObj(K key) {
+        this.keyValue = MutablePair.of(key, null);
     }
 
     public K getKey() {
-        return this.key;
+        //return this.key;
+        return this.keyValue.getKey();
     }
 
-    public void setKey(K key) {
-        this.key = key;
-    }
+    //public void setKey(K key) {
+    //    this.key = key;
+    //}
 
     public V getValue() {
-        return this.value;
+        //return this.value;
+        return this.keyValue.getValue();
     }
 
     public void setValue(V value) {
-        this.value = value;
+        //this.value = value;
+        this.keyValue.setValue(value);
     }
 
 }
